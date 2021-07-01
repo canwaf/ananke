d38309ccaa77287fb078fd4d40c0640abc45e9fe
Andrew Fergusson <andrew.fergusson@me.com>
2021-07-01 15:04:13 +0000

    new get_sparql() closes #7

--- ananke/download.py
+++ ananke/download.py
@@ -1,7 +1,12 @@
 # download.py
 import pandas as pd
+import hashlib
+import sqlite3
 
-from SPARQLWrapper import SPARQLWrapper, JSON, CSV
+from datetime import datetime
+
+from SPARQLWrapper import SPARQLWrapper, JSON
+
 
 def get_endpoint() -> str:
     """
@@ -15,22 +20,70 @@ def get_endpoint() -> str:
 
     sparql.setReturnFormat(JSON)
 
-    return  sparql
+    return sparql
 
-def json_to_df(results_json: dict) -> pd.DataFrame():
-    return pd.json_normalize(results_json['results']['bindings'])
+
+def get_sparql(query_text: str) -> pd.DataFrame:
+    """
+    Queries the set SPARQL endpoint the given query_text or returns a sqlite3
+    cached version. It creates a new sqlite3 database per SPARQL endpoint, and
+    there is no cache expiry. If you want to fetch fresh data, delete the
+    database. Soz.
+
+    Parameters:
+        query_text (str): a SPARQL query
+
+    Returns:
+        results (DataFrame): the results in a pandas DataFrame.
+    """
+    # Really bad caching, but I digress
+    query_hex = hashlib.sha224(query_text.encode()).hexdigest()
+    endpoint_hex = hashlib.sha224(get_endpoint().endpoint.encode()).hexdigest()
+
+    # find the database connection
+    con = sqlite3.connect(f"{endpoint_hex}.db")
+
+    # check if the query_hex exists
+    exist_query = f"SELECT count(*) FROM sqlite_master WHERE type='table' AND name='{query_hex}';"
+    exist_result = bool(con.execute(exist_query).fetchone()[0])  # 0 = False
+
+    if exist_result:
+        # results exist so fetch results
+        df = pd.read_sql(f"SELECT * FROM [{query_hex}]", con=con)
+    else:
+        # results don't exist in db so get them from SPARQL
+        sparql = get_endpoint()
+        sparql.setQuery(query_text)
+        results = sparql.queryAndConvert()
+
+        # normalise the json results
+        df = pd.json_normalize(results['results']['bindings'])
+
+        # store the results in db
+        df.to_sql(query_hex, con)
+
+        # log the query
+        record = {
+            "hash": [query_hex],
+            "query": [query_text],
+            "timestamp": [datetime.now().strftime("%Y-%m-%d %H:%M:%S")],
+        }
+        pd.DataFrame.from_dict(record, orient="columns").to_sql(
+            "manifest", con, if_exists="append", index=False
+        )
+
+    return df
+
 
 def get_datasets() -> dict:
     """
     Function to get the datasets (i.e. qb:cubes) for the SPARQL endpoint.
 
     Returns:
-        json (dict): SPARQL endpoint response, not normalised
+        results (DataFrame): The list of qb:Cubes on a SPARQL endpoint.
     """
 
-    query = \
-"""
-PREFIX pmdcat: <http://publishmydata.com/pmdcat#>
+    query = """PREFIX pmdcat: <http://publishmydata.com/pmdcat#>
 PREFIX dcterms: <http://purl.org/dc/terms/>
 
 # Select details regarding all datasets, assume that if there's a catalog entry
@@ -43,14 +96,10 @@ WHERE {
               dcterms:description ?catDesc ;
               pmdcat:datasetContents ?datasetUrl .
 
-}
-"""
+}"""
 
-    sparql = get_endpoint()
-    sparql.setQuery(query)
-    results = sparql.queryAndConvert()
+    return get_sparql(query)
 
-    return results
 
 def get_components(graph_uri: str):
     """
@@ -60,21 +109,11 @@ def get_components(graph_uri: str):
         graph_uri (str): string URI of for the graph containing the qb:cube
 
     Returns:
-        json (dict): SPARQL endpoint response, not normalised
+        results (DataFrame): The components of a qb:Cube
     """
 
-    query = \
-f"""
-PREFIX dcat: <http://www.w3.org/ns/dcat#>
-PREFIX pmdcat: <http://publishmydata.com/pmdcat#>
-PREFIX dcterms: <http://purl.org/dc/terms/>
-PREFIX owl: <http://www.w3.org/2002/07/owl#>
-PREFIX qb: <http://purl.org/linked-data/cube#>
+    query = f"""PREFIX qb: <http://purl.org/linked-data/cube#>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-PREFIX void: <http://rdfs.org/ns/void#>
-PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
 # For a given cube's graph, select its components
 
@@ -86,14 +125,10 @@ WHERE {{
         ?definition rdf:type ?kind .
     }}
 
-}}
-"""
+}}"""
 
-    sparql = get_endpoint()
-    sparql.setQuery(query)
-    results = sparql.queryAndConvert()
+    return get_sparql(query)
 
-    return results
 
 # Compare dimensions
 # Want to use urllib's object model to get the fragments of a url to find out
