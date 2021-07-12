792ab0789178e27e13dbd9d048a00f57b57e7581
Andrew Fergusson <andrew.fergusson@me.com>
2021-07-12 07:06:05 +0000

    Updates to exploration, cubes.py

--- ananke/cubes.py
+++ ananke/cubes.py
@@ -1,34 +1,67 @@
-from abc import ABC, abstractmethod
-from download import *
+from abc import ABC
+import datetime
+
+from ananke.download import get_sparql
+from typing import List
+from pandas import DataFrame
 
 class Component(ABC):
-    url: str
+    """ Basic class of components of the structure of a Cube.
 
+    uri (str) - the uri of the component
+    label (str) - the human readable label
+    type (str) - the type of component (qb spec)
+    """
 
     def __init__(self, url: str):
-        self.url = url
+        self.url: str = url
+        self.label: str
+        self.type: str
 
         # get the labels
+        pass
 
     def get_component_metadata(self):
         pass
-        
 
-    def label(self):
-        get_sparql(f"""
-        
-        
-        """)
-        pass
 
+class Attribute(Component):
+    """ An attribute is a component with no associated links, specific to the dataset.
+    
+    """
+    pass
     
 
 class Dimension(Component):
-
+    """ A dimension is a component of the dataset which can be linked to other datasets.
+    
+    dimension (str) - the uri of the dimension
+    subPropertyOf (str) - the parent linked data uri (normally refArea/refPeriod)
+    codelist (dict) - the dictionary of the codelist {code: human readable}
+    """
     def __init__(self, url: str):
+        self.dimension: str
+        self.subPropertyOf: str
+        self.codelist: dict
+
+
+class Dataset(ABC):
+    def __init__(self, url: str):
+        self.url: str = url
+
+        # Metadata
+        self.title: str # DCAT
+        self.description: str # DCAT
+
+        self.issued: datetime # DCAT
+        self.modified: datetime # DCAT
+
+        self.publisher: str # DCAT
+        self.creator: str # DCAT
+        self.comment: str # DCAT
+
+        self.components: List[Dimension, Attribute] # QB 
+        self.data: DataFrame # QB
+    
+    def fetch_attributes(self):
         pass
-
-    pass
-
-class DataSet(ABC):
-
